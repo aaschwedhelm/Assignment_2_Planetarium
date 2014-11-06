412198dae7377a3219a208ce6b7cb125ec891de3
Alejandro <aaschwedhelm@users.noreply.github.com>
2014-11-06 13:10:05 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 $('.myButton1').on('click',function(){
- $('#image').css('background','url(http://img1.wikia.nocookie.net/__cb20130707180853/vampirediaries/images/c/c9/Moon.jpg)').css('background-size', 'cover').css('background-position', 'center');
+ $('#image').css('background','url(http://img1.wikia.nocookie.net/__cb20130707180853/vampirediaries/images/c/c9/Moon.jpg)').css('background-size', 'cover').css('background-position', 'center').css('background-size','contain');
 });
 
 $('.myButton2').on('click',function(){
-    $('#image').css('background','url(http://www.paragonrelocation.com/blog/wp-content/uploads/2011/03/earth-space-709498.jpg)').css('background-size', 'cover').css('background-position', 'center');
+    $('#image').css('background','url(http://www.paragonrelocation.com/blog/wp-content/uploads/2011/03/earth-space-709498.jpg)').css('background-size', 'cover').css('background-position', 'center').css('background-size','contain');
 });
 
 $('.myButton3').on('click',function(){
- $('#image').css('background','url(http://upload.wikimedia.org/wikipedia/commons/2/25/Saturn_PIA06077.jpg)').css('background-size', 'cover').css('background-position', 'center');
+ $('#image').css('background','url(http://upload.wikimedia.org/wikipedia/commons/2/25/Saturn_PIA06077.jpg)').css('background-size', 'cover').css('background-position', 'center').css('background-size','contain');
 });
 
 $('.myButton4').on('click',function(){
- $('#image').css('background','url(http://thaiastro.nectec.or.th/activity/meeting/img/201302/jupiter.gif)').css('background-size', 'cover').css('background-position', 'center');
+ $('#image').css('background','url(http://thaiastro.nectec.or.th/activity/meeting/img/201302/jupiter.gif)').css('background-size', 'cover').css('background-position', 'center').css('background-size','contain');
 });
 
 $('.myButton5').on('click',function(){
-    $('#image').css('background','url(http://www.link2universe.net/wp-content/uploads/2012/04/Venere-Pioneer.jpg)').css('background-size', 'cover').css('background-position', 'center');
+    $('#image').css('background','url(http://www.link2universe.net/wp-content/uploads/2012/04/Venere-Pioneer.jpg)').css('background-size', 'cover').css('background-position', 'center').css('background-size','contain');
 });
 
 $('.myButton6').on('click',function(){
- $('#image').css('background','url(http://www.writerscafe.org/uploads/stories/ba4f1febb3743cf2455a641c91f11b4f.jpg)').css('background-size', 'cover').css('background-position', 'center');
+ $('#image').css('background','url(http://www.writerscafe.org/uploads/stories/ba4f1febb3743cf2455a641c91f11b4f.jpg)').css('background-size', 'cover').css('background-position', 'center').css('background-size','contain');
 });
